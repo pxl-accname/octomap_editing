854ea83e74cc0b039069204c0812a0fc9a1355fd
cmd-accname <mariusbauer91@gmail.com>
2019-11-18 11:44:33 +0100

    deleted the catkin_ws folder

--- catkin_ws/devel/share/catkin_tools_prebuild/cmake/catkin_tools_prebuildConfig.cmake
+++
@@ -1,1 +0,0 @@
-/home/accname/programming/ROS/octomap_editing/catkin_ws/devel/.private/catkin_tools_prebuild/share/catkin_tools_prebuild/cmake/catkin_tools_prebuildConfig.cmake
--- catkin_ws/devel/share/octomap_editing/cmake/octomap_editingConfig-version.cmake
+++
@@ -1,1 +0,0 @@
-/home/accname/programming/ROS/octomap_editing/catkin_ws/devel/.private/octomap_editing/share/octomap_editing/cmake/octomap_editingConfig-version.cmake
