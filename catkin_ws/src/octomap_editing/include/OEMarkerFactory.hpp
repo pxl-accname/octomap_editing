42a45c5fdab12d99ef106ad57b6b5065e5b17b3b
cmd-accname <mariusbauer91@gmail.com>
2019-11-18 11:46:16 +0100

    asdasd

---
+++ catkin_ws/src/octomap_editing/include/OEMarkerFactory.hpp
@@ -0,0 +1,45 @@
+#ifndef OEMARKERFACTORY_HPP
+#define OEMARKERFACTORY_HPP
+
+#include <visualization_msgs/Marker.h>
+#include <visualization_msgs/InteractiveMarker.h>
+#include <visualization_msgs/InteractiveMarkerControl.h>
+#include <octomap/ColorOcTree.h>
+#include <OEMarker.hpp>
+
+namespace octomap_editing
+{
+  // TODO: make this factory a singleton!
+  class OEMarkerFactory
+  {
+  public:
+    OEMarkerFactory(double resolution);
+
+    // function to create the control marker
+    visualization_msgs::InteractiveMarker createControlMarker();
+    visualization_msgs::Marker createOriginMarker();
+    visualization_msgs::InteractiveMarker createTextMarker();
+
+    // different possible control types - added via OEServer
+    visualization_msgs::InteractiveMarkerControl createMoveAxisControl(std::string axis);
+    visualization_msgs::InteractiveMarkerControl createRotateAxisControl(std::string axis);
+
+    // needed by OEServer
+    std::string formatText(double x = 0, double y = 0, double z = 0, double roll = 0, double pitch = 0, double yaw = 0);
+    uint getNextSeq() { return _marker_count++; }
+
+  private:
+    void setResolution(double resolution) { _resolution = resolution; }
+
+    // used to create parts of the possible markers
+    std_msgs::Header makeMarkerHeader();
+    geometry_msgs::Pose makeMarkerPose();
+    visualization_msgs::Marker makeMarker(uint type = visualization_msgs::Marker::CUBE, std::string text = "");
+    visualization_msgs::InteractiveMarkerControl makeControl(uint control_mode = visualization_msgs::InteractiveMarkerControl::NONE, std::string name = "");
+
+    uint _marker_count = 0;
+    double _resolution;
+  };
+}
+
+#endif // OEMARKERFACTORY_HPP
