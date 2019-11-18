ee59fd60825364cc572e91531c8c1741212d4569
cmd-accname <mariusbauer91@gmail.com>
2019-11-18 11:50:02 +0100

    asdad

---
+++ catkin_ws/src/octomap_editing/src/OEMarkerFactory.cpp
@@ -0,0 +1,182 @@
+#include <OEMarkerFactory.hpp>
+
+namespace octomap_editing
+{
+  OEMarkerFactory::OEMarkerFactory(double resolution)
+    : _resolution(resolution)
+  {}
+
+  // first type of marker
+  visualization_msgs::InteractiveMarker
+  OEMarkerFactory::createControlMarker()
+  {
+    visualization_msgs::InteractiveMarker imarker;
+    imarker.header = makeMarkerHeader();
+    imarker.pose = makeMarkerPose();
+    imarker.name = "imarker_pointcloud";  // + std::to_string(imarker.header.seq);
+
+    visualization_msgs::InteractiveMarkerControl menu = makeControl(visualization_msgs::InteractiveMarkerControl::MENU);
+    menu.markers.push_back(makeMarker(visualization_msgs::Marker::CUBE, ""));
+    imarker.controls.push_back(menu);
+    return imarker;
+  }
+
+  // second type of marker
+  visualization_msgs::InteractiveMarker
+  OEMarkerFactory::createTextMarker()
+  {
+    //lets try to create a text marker
+    visualization_msgs::InteractiveMarker imarker_text;
+    imarker_text.header = makeMarkerHeader();
+    imarker_text.pose = makeMarkerPose();
+    imarker_text.name = "imarker_text"; //_" + std::to_string(imarker_text.header.seq);
+
+    visualization_msgs::InteractiveMarkerControl control = makeControl(visualization_msgs::InteractiveMarkerControl::NONE);
+    std::string text = formatText(0, 0, 0, 0, 0, 0);
+    control.markers.push_back(makeMarker(visualization_msgs::Marker::TEXT_VIEW_FACING, text));
+    imarker_text.controls.push_back(control);
+    return imarker_text;
+  }
+
+  // third type of marker
+  visualization_msgs::Marker
+  OEMarkerFactory::createOriginMarker()
+  {
+    visualization_msgs::Marker help_marker;
+    help_marker.action = visualization_msgs::Marker::ADD;
+    help_marker.color.r = 0.0;
+    help_marker.color.g = 1.0;
+    help_marker.color.b = 0.0;
+    help_marker.color.a = 1.0;
+    help_marker.header.stamp = ros::Time::now();
+    help_marker.header.frame_id = "map";
+    help_marker.header.seq = 999;
+    geometry_msgs::Point p;
+    p.x = p.y = 0.0;
+    p.z = 1.0;
+    help_marker.points.push_back(p);
+    p.z = -3.0;
+    help_marker.points.push_back(p);
+    help_marker.type = visualization_msgs::Marker::ARROW;
+    help_marker.scale.x = 0.5; // scale.x is the shaft diameter
+    help_marker.scale.y = 0.5; // scale.y is the head diameter
+    help_marker.scale.z = 0.25; // If scale.z is not zero, it specifies the head length.
+  }
+
+  geometry_msgs::Pose
+  OEMarkerFactory::makeMarkerPose()
+  {
+    geometry_msgs::Pose pose;
+    pose.position.x = 0;
+    pose.position.y = 0;
+    pose.position.z = 0;
+    pose.orientation.x = 0.0;
+    pose.orientation.y = 0.0;
+    pose.orientation.z = 0.0;
+    pose.orientation.w = 1.0;
+    return pose;
+  }
+
+  visualization_msgs::Marker
+  OEMarkerFactory::makeMarker(uint type, std::string text)
+  {
+    visualization_msgs::Marker marker;
+    marker.type = type;
+    marker.scale.x = 4 * _resolution;
+    marker.scale.y = 4 * _resolution;
+    marker.scale.z = 4 * _resolution;
+    marker.color.r = 1.0;
+    marker.color.g = 0.0;
+    marker.color.b = 0.0;
+    marker.color.a = 1.0;
+    marker.text = text;
+    return marker;
+  }
+
+  std_msgs::Header
+  OEMarkerFactory::makeMarkerHeader()
+  {
+    std_msgs::Header header; // TODO: make this an argument
+    header.frame_id = "map";  // TODO: make this an argument
+    header.stamp = ros::Time::now();
+    header.seq = _marker_count++;
+    return header;
+  }
+
+  visualization_msgs::InteractiveMarkerControl
+  OEMarkerFactory::makeControl(uint control_mode, std::string name)
+  {
+    visualization_msgs::InteractiveMarkerControl control;
+    control.interaction_mode = control_mode;
+    control.name = name;
+    control.always_visible = true;
+    return control;
+  }
+
+  std::string
+  OEMarkerFactory::formatText(double x , double y, double z, double roll, double pitch, double yaw)
+  {
+    std::stringstream text;
+    text << "x: " << x << std::endl;
+    text << "y: " << y << std::endl;
+    text << "z: " << z << std::endl << std::endl;
+
+    text << "roll: " << roll << std::endl;
+    text << "pitch: " << pitch << std::endl;
+    text << "yaw: " << yaw;
+    return text.str();
+  }
+
+  visualization_msgs::InteractiveMarkerControl
+  OEMarkerFactory::createMoveAxisControl(std::string axis)
+  {
+    double x = 0.0, y = 0.0, z = 0.0;
+
+    if (axis == "move_X")
+      x = 1.0;
+
+    if (axis == "move_Y")
+      z = 1.0;
+
+    if (axis == "move_Z")
+      y = 1.0;
+
+    visualization_msgs::InteractiveMarkerControl interactive_control;
+    interactive_control.name = axis;
+    interactive_control.interaction_mode = visualization_msgs::InteractiveMarkerControl::MOVE_AXIS;
+    interactive_control.orientation_mode = visualization_msgs::InteractiveMarkerControl::FIXED;
+    interactive_control.orientation.x = x;
+    interactive_control.orientation.y = y;
+    interactive_control.orientation.z = z;
+    interactive_control.orientation.w = 1;
+    interactive_control.always_visible = true;
+    return interactive_control;
+  }
+
+  visualization_msgs::InteractiveMarkerControl
+  OEMarkerFactory::createRotateAxisControl(std::string axis)
+  {
+    double x = 0.0, y = 0.0, z = 0.0;
+
+    if (axis == "rotate_X")
+      x = 1.0;
+
+    if (axis == "rotate_Y")
+      z = 1.0;
+
+    if (axis == "rotate_Z")
+      y = 1.0;
+
+    visualization_msgs::InteractiveMarkerControl interactive_control;
+    interactive_control.name = axis;
+    interactive_control.interaction_mode = visualization_msgs::InteractiveMarkerControl::ROTATE_AXIS;
+    interactive_control.orientation_mode = visualization_msgs::InteractiveMarkerControl::FIXED;
+    interactive_control.orientation.x = x;
+    interactive_control.orientation.y = y;
+    interactive_control.orientation.z = z;
+    interactive_control.orientation.w = 1;
+    interactive_control.always_visible = true;
+    return interactive_control;
+  }
+
+}
---
+++ catkin_ws/src/octomap_editing/src/OEServer.cpp
@@ -0,0 +1,429 @@
+#include <OEServer.hpp>
+#include <map>
+
+
+using namespace interactive_markers;
+
+namespace octomap_editing
+{
+
+  OEServer::OEServer(double resolution, std::string mapFilename, std::string update_topic)
+    : _ocTree(resolution),
+      _mapFilename(mapFilename),
+      _server(update_topic, "MarkerServer", true),
+      _markerFactory(resolution),
+      _nh(ros::NodeHandle("octomap_editing")),
+      _ocPointcloud()
+  {
+    m_publishFreeSpace = true;
+
+    openMapfile();
+
+    // initially needed markers
+    createControlMarker();
+    createTextMarker();
+
+    _sub = _nh.subscribe("/octomap_point_cloud_centers", 1000, &octomap_editing::OEServer::getPointCloudCallback, this);
+    _marker_pub = _nh.advertise<visualization_msgs::Marker>("/visualization_marker", 10);
+
+    _server.applyChanges();
+    // maybe even refreshServer()
+  }
+
+  void
+  OEServer::openMapfile()
+  {
+    std::cout << "Opening map file..." << std::endl;
+    _ocTree.readBinary(_mapFilename);
+    openFile(_mapFilename);
+    std::cout << "Map file opened!" << std::endl;
+  }
+
+  void
+  OEServer::getPointCloudCallback(const sensor_msgs::PointCloud2 &pc)
+  {
+    if(_ocPointcloud.size() == 0)
+    {
+      octomap::pointCloud2ToOctomap(pc, _ocPointcloud);
+      _sub.shutdown();
+      _pub = _nh.advertise<sensor_msgs::PointCloud2>("/octomap_point_cloud_centers2", 1000);
+      publishPointCloud();
+    }
+  }
+
+  void
+  OEServer::publishPointCloud()
+  {
+    sensor_msgs::PointCloud2 msg;
+    msg.header.frame_id = "map";
+    msg.header.stamp = ros::Time::now();
+    msg.header.seq = _markerFactory.getNextSeq();
+
+    octomap::point3d_list p3_list;
+    octomath::Pose6D transformMatrix;
+
+    // TODO: this is ugly...
+    transformMatrix = getTransformationMatrix(true);
+    _ocPointcloud.transform(transformMatrix);
+    transformMatrix = getTransformationMatrix(false);
+    _ocPointcloud.rotate(transformMatrix.roll(), transformMatrix.pitch(), transformMatrix.yaw());
+
+    for (auto it = _ocPointcloud.begin(), end = _ocPointcloud.end(); it != end; ++it)
+    {
+      p3_list.push_back(octomath::Vector3(it->x(), it->y(), it->z()));
+    }
+
+    sensor_msgs::PointCloud2Modifier pcd_modifier(msg);
+    pcd_modifier.setPointCloud2FieldsByString(1, "xyz");
+    octomap::pointsOctomapToPointCloud2(p3_list, msg);
+    _pub.publish(msg);
+  }
+
+  interactive_markers::MenuHandler
+  OEServer::createMenu()
+  {
+    interactive_markers::MenuHandler menu_handler;
+
+    // parent menu entries
+    interactive_markers::MenuHandler::EntryHandle move_axis_entry = menu_handler.insert("Move Axis");
+    interactive_markers::MenuHandler::EntryHandle rotate_axis_entry = menu_handler.insert("Rotate Axis");
+    interactive_markers::MenuHandler::EntryHandle done_entry = menu_handler.insert("Apply changes!", boost::bind(&octomap_editing::OEServer::applyChangesFeedback, this, _1));
+    interactive_markers::MenuHandler::EntryHandle reset_entry = menu_handler.insert("Reset changes!", boost::bind(&octomap_editing::OEServer::resetChangesFeedback, this, _1));
+
+    // move part
+    interactive_markers::MenuHandler::EntryHandle move_axis_x_entry = menu_handler.insert(move_axis_entry, "X", boost::bind(&octomap_editing::OEServer::menuMoveAxisFeedback, this, _1));
+    menu_handler.setCheckState(move_axis_x_entry, MenuHandler::UNCHECKED);
+    _mapping_menuentry_axis[move_axis_x_entry] = "X";
+
+
+    interactive_markers::MenuHandler::EntryHandle move_axis_y_entry = menu_handler.insert(move_axis_entry, "Y", boost::bind(&octomap_editing::OEServer::menuMoveAxisFeedback, this, _1));
+    menu_handler.setCheckState(move_axis_y_entry, MenuHandler::UNCHECKED);
+    _mapping_menuentry_axis[move_axis_y_entry] = "Y";
+
+
+    interactive_markers::MenuHandler::EntryHandle move_axis_z_entry = menu_handler.insert(move_axis_entry, "Z", boost::bind(&octomap_editing::OEServer::menuMoveAxisFeedback, this, _1));
+    menu_handler.setCheckState(move_axis_z_entry, MenuHandler::UNCHECKED);
+    _mapping_menuentry_axis[move_axis_z_entry] = "Z";
+
+
+    // rotate part
+    interactive_markers::MenuHandler::EntryHandle rotate_axis_x_entry = menu_handler.insert(rotate_axis_entry, "X", boost::bind(&octomap_editing::OEServer::menuRotateAxisFeedback, this, _1));
+    menu_handler.setCheckState(rotate_axis_x_entry, MenuHandler::UNCHECKED);
+    _mapping_menuentry_axis[rotate_axis_x_entry] = "X";
+
+
+    interactive_markers::MenuHandler::EntryHandle rotate_axis_y_entry = menu_handler.insert(rotate_axis_entry, "Y", boost::bind(&octomap_editing::OEServer::menuRotateAxisFeedback, this, _1));
+    menu_handler.setCheckState(rotate_axis_y_entry, MenuHandler::UNCHECKED);
+    _mapping_menuentry_axis[rotate_axis_y_entry] = "Y";
+
+
+    interactive_markers::MenuHandler::EntryHandle rotate_axis_z_entry = menu_handler.insert(rotate_axis_entry, "Z", boost::bind(&octomap_editing::OEServer::menuRotateAxisFeedback, this, _1));
+    menu_handler.setCheckState(rotate_axis_z_entry, MenuHandler::UNCHECKED);
+    _mapping_menuentry_axis[rotate_axis_z_entry] = "Z";
+
+    return menu_handler;
+  }
+
+  void
+  OEServer::createControlMarker()
+  {
+    // creates the interactive marker
+    visualization_msgs::InteractiveMarker control_marker = _markerFactory.createControlMarker();
+    _markers[control_marker.name] = std::make_shared<visualization_msgs::InteractiveMarker>(control_marker);
+
+    // creates the menu
+    interactive_markers::MenuHandler menu_handler = createMenu();
+    _menus[control_marker.name] = std::make_shared<interactive_markers::MenuHandler>(menu_handler);
+
+    // insert marker with callback into the server
+    _server.insert(*_markers[control_marker.name], boost::bind(&octomap_editing::OEServer::processMarkerFeedback, this, _1));
+
+    // apply the menu handle to the control marker
+    _menus[control_marker.name]->apply(_server, control_marker.name);
+    _pointcloud_pose_change = control_marker.pose;
+
+//    _imarker_pose_initial = _imarker.pose;
+  }
+
+  void
+  OEServer::createTextMarker()
+  {
+    visualization_msgs::InteractiveMarker text_marker = _markerFactory.createTextMarker();
+    _markers[text_marker.name] = std::make_shared<visualization_msgs::InteractiveMarker>(text_marker);
+    _server.insert(*_markers[text_marker.name]);
+  }
+
+  void
+  OEServer::menuMoveAxisFeedback(const visualization_msgs::InteractiveMarkerFeedbackConstPtr &feedback)
+  {
+    MenuHandler::CheckState check_state;
+    std::shared_ptr<visualization_msgs::InteractiveMarker> marker = _markers[feedback->marker_name];
+    std::shared_ptr<interactive_markers::MenuHandler> menu = _menus[marker->name];
+    menu->getCheckState(feedback->menu_entry_id, check_state);
+
+    if (check_state == MenuHandler::CHECKED)
+    {
+      menu->setCheckState(feedback->menu_entry_id, MenuHandler::UNCHECKED);
+      std::string name = "move_";
+      name += _mapping_menuentry_axis[feedback->menu_entry_id];
+      for (auto it = marker->controls.begin(), end = marker->controls.end(); it != end; ++it)
+      {
+        if (it->name == name)
+        {
+          marker->controls.erase(it);
+        }
+      }
+    }
+    else
+    {
+      menu->setCheckState(feedback->menu_entry_id, MenuHandler::CHECKED);
+      std::string title;
+      menu->getTitle(feedback->menu_entry_id, title);
+      marker->controls.push_back(_markerFactory.createMoveAxisControl("move_" + title));
+    }
+    refreshServer();
+  }
+
+  void
+  OEServer::menuRotateAxisFeedback(const visualization_msgs::InteractiveMarkerFeedbackConstPtr &feedback)
+  {
+    MenuHandler::CheckState check_state;
+    std::shared_ptr<visualization_msgs::InteractiveMarker> marker = _markers[feedback->marker_name];
+    std::shared_ptr<interactive_markers::MenuHandler> menu = _menus[marker->name];
+    menu->getCheckState(feedback->menu_entry_id, check_state);
+
+    if (check_state == MenuHandler::CHECKED)
+    {
+      menu->setCheckState(feedback->menu_entry_id, MenuHandler::UNCHECKED);
+      std::string name = "rotate_";
+      name += _mapping_menuentry_axis[feedback->menu_entry_id];
+      for (auto it = marker->controls.begin(), end = marker->controls.end(); it != end; ++it)
+      {
+        if (it->name == name)
+        {
+          marker->controls.erase(it);
+        }
+      }
+    }
+    else
+    {
+      menu->setCheckState(feedback->menu_entry_id, MenuHandler::CHECKED);
+      std::string title;
+      menu->getTitle(feedback->menu_entry_id, title);
+      marker->controls.push_back(_markerFactory.createRotateAxisControl("rotate_" + title));
+    }
+    refreshServer();
+  }
+
+  void
+  OEServer::refreshServer()
+  {
+    for (auto it = _markers.begin(), end = _markers.end(); it != end; ++it)
+    {
+      _server.insert(*(it->second));
+    }
+
+    for (auto it = _menus.begin(), end = _menus.end(); it != end; ++it)
+    {
+      (*(it->second)).reApply(_server);
+    }
+    // _server.setPose(_imarker_text.name, _imarker_text.pose); --> for the text marker!
+    _server.applyChanges();
+  }
+
+  void
+  OEServer::calculatePoseChange(geometry_msgs::Pose new_pose)
+  {
+    std::shared_ptr<visualization_msgs::InteractiveMarker> imarker = _markers["imarker_pointcloud"];
+    _pointcloud_pose_change.position.x = (imarker->pose.position.x != new_pose.position.x) ? new_pose.position.x - imarker->pose.position.x : 0;
+    _pointcloud_pose_change.position.y = (imarker->pose.position.y != new_pose.position.y) ? new_pose.position.y - imarker->pose.position.y : 0;
+    _pointcloud_pose_change.position.z = (imarker->pose.position.z != new_pose.position.z) ? new_pose.position.z - imarker->pose.position.z : 0;
+
+    _pointcloud_pose_change.orientation.w = 1;
+    _pointcloud_pose_change.orientation.x = (imarker->pose.orientation.x != new_pose.orientation.x) ? new_pose.orientation.x - imarker->pose.orientation.x : 0;
+    _pointcloud_pose_change.orientation.y = (imarker->pose.orientation.y != new_pose.orientation.y) ? new_pose.orientation.y - imarker->pose.orientation.y : 0;
+    _pointcloud_pose_change.orientation.z = (imarker->pose.orientation.z != new_pose.orientation.z) ? new_pose.orientation.z - imarker->pose.orientation.z : 0;
+  }
+
+
+  void
+  OEServer::processMarkerFeedback(const visualization_msgs::InteractiveMarkerFeedbackConstPtr &feedback )
+  {
+    if (feedback->event_type == visualization_msgs::InteractiveMarkerFeedback::MOUSE_DOWN)
+    {
+
+    }
+    else if (feedback->event_type == visualization_msgs::InteractiveMarkerFeedback::MOUSE_UP)
+    {
+      if (_markers.find(feedback->marker_name) != _markers.end())
+      {
+        if (feedback->marker_name == "imarker_pointcloud")
+        {
+          // calculate the change in position
+          calculatePoseChange(feedback->pose);
+          publishPointCloud();
+        }
+
+        std::shared_ptr<visualization_msgs::InteractiveMarker> imarker = _markers[feedback->marker_name];
+        imarker->pose = feedback->pose;
+
+        updateText(feedback->pose);
+        refreshServer();
+      }
+      else
+      {
+        // marker nicht gefunden
+      }
+    }
+  }
+
+
+
+
+
+
+
+  void
+  OEServer::applyChangesFeedback(const visualization_msgs::InteractiveMarkerFeedbackConstPtr &feedback)
+  {
+    octomath::Pose6D transformMatrix_t = getTransformationMatrix(true);
+    octomath::Pose6D transformMatrix_r = getTransformationMatrix(false);
+    bool occupied = false;
+    octomap::point3d point;
+    octomap::ColorOcTree ocTree(0.05);
+    //octomap::ColorOcTreeNode::Color nodeColor;
+
+    for (auto it = _ocTree.begin_tree(), end = _ocTree.end_tree(); it != end; ++it)
+    {
+      // delete old node
+      point = it.getCoordinate();
+      occupied = (_ocTree.isNodeOccupied(*it)) ? true : false;
+
+      point += transformMatrix_t.trans();
+      point.rotate_IP(transformMatrix_r.roll(), transformMatrix_r.pitch(), transformMatrix_r.yaw());
+      ocTree.updateNode(point, occupied);
+      //ocTree.setNodeColor(point.x(), point.y(), point.z(), nodeColor.r, nodeColor.g, nodeColor.b);
+    }
+
+    ocTree.writeBinary("/home/accname/programming/ROS/octomap_editing/fr_transformed.bt"); // TODO: name of saved tree
+    std::cout << "Transformed OcTree saved!" << std::endl;
+  }
+
+  octomap::pose6d
+  OEServer::getTransformationMatrix(bool transl)
+  {
+    if (transl)
+    {
+      octomath::Vector3 trans(static_cast<float>(_pointcloud_pose_change.position.x),
+                               static_cast<float>(_pointcloud_pose_change.position.y),
+                               static_cast<float>(_pointcloud_pose_change.position.z));
+      octomath::Quaternion rotat(static_cast<float>(1),
+                                 static_cast<float>(0),
+                                 static_cast<float>(0),
+                                 static_cast<float>(0));
+      octomap::pose6d transformMatrix(trans, rotat);
+      return transformMatrix;
+    }
+    else
+    {
+      octomath::Vector3 trans(static_cast<float>(0),
+                               static_cast<float>(0),
+                               static_cast<float>(0));
+      octomath::Quaternion rotat(static_cast<float>(1),
+                                 static_cast<float>(_pointcloud_pose_change.orientation.x),
+                                 static_cast<float>(_pointcloud_pose_change.orientation.y),
+                                 static_cast<float>(_pointcloud_pose_change.orientation.z));
+      octomap::pose6d transformMatrix(trans, rotat);
+      return transformMatrix;
+    }
+  }
+
+  void
+  OEServer::updateText(geometry_msgs::Pose pose)
+  {
+    _server.erase("imarker_text");
+    octomath::Vector3 v(pose.position.x, pose.position.y, pose.position.z);
+    octomath::Quaternion q(pose.orientation.w, pose.orientation.x, pose.orientation.y, pose.orientation.z);
+    octomath::Pose6D p(v,q);
+
+    std::shared_ptr<visualization_msgs::InteractiveMarker> imarker_text = _markers["imarker_text"];
+    imarker_text->controls[0].markers[0].text = _markerFactory.formatText(p.x(), p.y(), p.z(), p.roll(), p.pitch(), p.yaw());
+    imarker_text->pose = pose;
+    _server.insert(*imarker_text);
+    refreshServer();
+  }
+
+
+
+  void
+  OEServer::resetChangesFeedback(const visualization_msgs::InteractiveMarkerFeedbackConstPtr &feedback)
+  {
+    // reset the imarker and the pointcloud
+    // for the pointcloud --> take current pose and calc the change
+    // for the marker --> set the correct pose
+
+    //careful: this is an ugly hack!
+    geometry_msgs::Point p;
+    p.x = 0.;
+    p.y = 0.;
+    p.z = 0.;
+    geometry_msgs::Quaternion q;
+    q.w = 1.;
+    q.x = 0.;
+    q.y = 0.;
+    q.z = 0.;
+    geometry_msgs::Pose po;
+    po.position = p;
+    po.orientation = q;
+
+    std::shared_ptr<visualization_msgs::InteractiveMarker> imarker = _markers["imarker_pointcloud"];
+
+
+    calculatePoseChange(po);
+    publishPointCloud();
+    updateText(po);
+    imarker->pose = po;
+
+    refreshServer();
+  }
+
+  void
+  OEServer::testLineMarker()
+  {
+
+    visualization_msgs::Marker line_list;
+    line_list.header.frame_id = "map";
+    line_list.header.stamp = ros::Time::now();
+    line_list.ns = "";
+    line_list.action = visualization_msgs::Marker::ADD;
+    line_list.pose.orientation.w = 1.0;
+    line_list.id = 2;
+    line_list.type = visualization_msgs::Marker::LINE_LIST;
+
+    // LINE_STRIP/LINE_LIST markers use only the x component of scale, for the line width
+    line_list.scale.x = 0.1;
+    // Line list is red
+    line_list.color.r = 1.0;
+    line_list.color.a = 1.0;
+
+
+
+    // Create the vertices for the points and lines
+    for (uint32_t i = 0; i < 10; ++i)
+    {
+      float y = 1;
+      float z = 1;
+
+      geometry_msgs::Point p;
+      p.x = (int32_t)i;
+      p.y = y;
+      p.z = z;
+
+      // The line list needs two points for each line
+      line_list.points.push_back(p);
+      p.z += 1.0;
+      line_list.points.push_back(p);
+    }
+    _marker_pub.publish(line_list);
+  }
+}
